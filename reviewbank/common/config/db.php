adffa290a992a259d536ff62ab442dba26412846
Irina <sokolovaie@altarix.ru>
2017-05-04 15:44:49 +0300

    Build yii2 advanced empty app

---
+++ reviewbank/common/config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'psql:host=localhost;dbname=reviewbank',
+            'username' => 'postgres',
+            'password' => '123456',
+            'charset' => 'utf8',
+        ];
